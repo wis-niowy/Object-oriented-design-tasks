90917d0b92c6734516849ed82b683b320e1b23f2
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:23:26 +0200

    Add files via upload

---
+++ lab3/Multiplatform/WP8/DialogWindowWP8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplatform.WP8
+{
+    class DialogWindowWP8: Interfaces.IDialogWindow
+    {
+        private string dialogName;
+        private List<Interfaces.ISlider> sliderList;
+        private List<Interfaces.IButton> buttonList;
+
+        public DialogWindowWP8(string name, List<Interfaces.ISlider> sliderList, List<Interfaces.IButton> buttonList)
+        {
+            this.dialogName = name;
+            this.sliderList = sliderList;
+            this.buttonList = buttonList;
+        }
+        public void DrawDialog()
+        {
+            Console.WriteLine("Drawing WP8 dialog: {0}", dialogName);
+            foreach (var slider in sliderList)
+            {
+                slider.DrawContent();
+            }
+            foreach (var button in buttonList)
+            {
+                button.DrawContent();
+            }
+        }
+    }
+}
---
+++ lab3/Multiplatform/WP8/SliderWP8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplatform.WP8
+{
+    class SliderWP8: Interfaces.ISlider
+    {
+        private float minValue;
+        private float maxValue;
+        private float incrementStep;
+
+        public SliderWP8(params float[] args)
+        {
+            minValue = args[0];
+            maxValue = args[1];
+            incrementStep = args[2];
+            CurrentValue = minValue;
+            Console.WriteLine("WP8 Slider created");
+        }
+        public float CurrentValue
+        {
+            get; private set;
+        }
+
+        public void ChangeValue(float value)
+        {
+            CurrentValue += value * incrementStep;
+            if (CurrentValue > maxValue) CurrentValue = maxValue;
+            if (CurrentValue < minValue) CurrentValue = minValue;
+            Console.WriteLine("New WP8 Slider value: {0}", CurrentValue);
+        }
+
+        public void DrawContent()
+        {
+            Console.WriteLine("PlatformName slider from {0} to {1} with step: {2}, current: {3}",
+                minValue, maxValue, incrementStep, CurrentValue);
+
+        }
+    }
+}
