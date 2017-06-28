23b96bde15f3adfbcf56226e2ca242330088e617
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:21:14 +0200

    Create IButton.cs

---
+++ lab3/Multiplatform/Interfaces/IButton.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplatform.Interfaces
+{
+	public interface IButton
+	{
+		void ButtonPressed();
+		void DrawContent();
+	}
+}
