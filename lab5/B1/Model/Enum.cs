1b9a4dc4cee13ff8c77357e9d5fea69fe65c0f9a
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:37:24 +0200

    Create Enum.cs

---
+++ lab5/B1/Model/Enum.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Model
+{
+    public enum HeroClass
+    {
+        Warrior,
+        Mage,
+        Warlock,
+        Shaman,
+        Paladin,
+        Priest
+    }
+
+    enum Race
+    {
+        Night_elf,
+        Human,
+        Dwarf,
+        Orc,
+        Troll
+    }
+
+    enum WeaponType
+    {
+        Bow,
+        Sword,
+        Dagger,
+        Mace,
+        Axe
+    }
+}
