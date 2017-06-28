ce08360bc583daaa9ae7cf08319665f489439045
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:24:46 +0200

    Add files via upload

---
+++ lab3/Multiplatform/XFCE/DialogWindowXFCE.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplatform.XFCE
+{
+    class DialogWindowXFCE: Interfaces.IDialogWindow
+    {
+        private string dialogName;
+        private List<Interfaces.ISlider> sliderList;
+        private List<Interfaces.IButton> buttonList;
+
+        public DialogWindowXFCE(string name, List<Interfaces.ISlider> sliderList, List<Interfaces.IButton> buttonList)
+        {
+            this.dialogName = name;
+            this.sliderList = sliderList;
+            this.buttonList = buttonList;
+        }
+        public void DrawDialog()
+        {
+            Console.WriteLine("XFCE dialog: {0}", dialogName);
+            foreach (var slider in sliderList)
+            {
+                slider.DrawContent();
+            }
+            foreach (var button in buttonList)
+            {
+                button.DrawContent();
+            }
+            Console.WriteLine("To accept press ^ O +^ X, to cancel press ESC");
+        }
+    }
+}
---
+++ lab3/Multiplatform/XFCE/SliderXFCE.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplatform.XFCE
+{
+    class SliderXFCE: Interfaces.ISlider
+    {
+        private float minValue;
+        private float maxValue;
+        private float incrementStep;
+
+        public SliderXFCE(params float[] args)
+        {
+            minValue = args[0];
+            maxValue = args[1];
+            incrementStep = args[2];
+            CurrentValue = minValue;
+            Console.WriteLine("XFCE Slider created");
+        }
+        public float CurrentValue
+        {
+            get; private set;
+        }
+
+        public void ChangeValue(float value)
+        {
+            CurrentValue += value * incrementStep;
+            CurrentValue = CurrentValue % (maxValue - minValue) + minValue;
+            Console.WriteLine("New XFCE Slider value: {0}", CurrentValue);
+        }
+
+        public void DrawContent()
+        {
+            Console.WriteLine("PlatformName slider from {0} to {1} with step: {2}, current: {3}",
+                minValue, maxValue, incrementStep, CurrentValue);
+
+        }
+    }
+}
