84f92a5299b49a576178f06750cb2681bbb39dc9
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:14:46 +0200

    Add files via upload

---
+++ lab2/Av2/RFM/ErrorMessageRFM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Serialization.RFM
+{
+    class ErrorMessageSF: Interfaces.IErrorMessage
+    {
+        public string Serialize()
+        {
+            string result = "Error encountered";
+            result = "{RFM}" + result.MyStringReverse() + "{RFM}";
+            return result;
+        }
+        
+    }
+    
+}
---
+++ lab2/Av2/RFM/OwnerReportRFM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization.RFM
+{
+    public class OwnerReportSF : Interfaces.IOwnerReport
+    {
+        public string OwnerName { private get; set; }
+        public string OwnerStatus { private get; set; }
+        public string Serialize()
+        {
+            string result = "{RFM}";
+            result += OwnerName.MyStringReverse() + "&" + OwnerStatus.MyStringReverse();
+            result += "{RFM}";
+            return result;
+        }
+    }
+
+}
