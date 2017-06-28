21744752ae0aa374578274909a837da82f07a3cd
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:42:02 +0200

    Add files via upload

---
+++ lab6/IV sem - ProjOb - lab8 - 3d/Objects/Segment3D.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+
+namespace Graphics3D
+{
+	public class Segment3D: WireframeObject3D
+	{
+		private double _lineWidth;
+
+		public Segment3D (Colorspace colorspace, double lineWidth) : base(colorspace)
+		{
+			_lineWidth = lineWidth;
+		}
+
+		public override string GetName ()
+		{
+			return "Segment3D";
+		}
+
+		public override double GetLineWidth()
+		{
+			return _lineWidth;
+		}
+
+        //
+        public override bool accept(Renderer renderer)
+        {
+            return renderer.draw(this);
+        }
+        public override bool canAcceptr(Renderer rend)
+        {
+            return rend.canDraw(this);
+        }
+    }
+}
+
---
+++ lab6/IV sem - ProjOb - lab8 - 3d/Objects/Triangle3D.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+
+namespace Graphics3D
+{
+	public class Triangle3D: FilledObject3D
+	{
+		private double _transparencyLevel;
+
+		public Triangle3D (Colorspace colorspace, double transparencyLevel) : base(colorspace)
+		{
+			_transparencyLevel = transparencyLevel;
+		}
+
+		public override string GetName ()
+		{
+			return "Triangle3D";
+		}
+
+		public override double GetTransparencyLevel()
+		{
+			return _transparencyLevel;
+		}
+
+        //
+
+        public override bool accept(Renderer renderer)
+        {
+            return renderer.draw(this);
+        }
+        public override bool canAcceptr(Renderer rend)
+        {
+            return rend.canDraw(this);
+        }
+    }
+}
