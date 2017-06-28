61047cfdb73bf8de9d95e5d6a77b45ab8a8a1090
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:15:35 +0200

    Add files via upload

---
+++ lab2/Av2/SF/FundsReportRFM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization.RFM
+{
+    class FundsReportSF: Interfaces.IFundsReport
+    {
+        public float FundsAmount { private get; set; }
+        public int AccountsCount { private get; set; }
+        public string Serialize()
+        {
+            string result = "{RFM}";
+            result += FundsAmount.ToString().MyStringReverse();
+            if (FundsAmount >= 0) result += "-";
+            result += "&";
+            result += AccountsCount.ToString().MyStringReverse();
+            if (AccountsCount >= 0) result += "-";
+            result += "{RFM}";
+            return result;
+        }
+    }
+}
