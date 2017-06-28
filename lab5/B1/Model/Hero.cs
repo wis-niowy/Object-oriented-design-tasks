27d92d9c7a96bae290bfc318ba0a0e58e750c2a1
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:37:38 +0200

    Add files via upload

---
+++ lab5/B1/Model/Hero.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Model
+{
+    class Hero
+    {
+        private readonly string _heroName;
+        private readonly HeroClass _hClass;
+        private readonly Race _race;
+        private readonly int id;
+
+        public int Hp { get; set; }
+        public int Mana { get; set; }
+        public int Lvl { get; set; }
+        public WeaponType Weapon { get; set; }
+        public int NMinions { get; set; }
+        public string Title { get; set; }
+
+
+        public Hero(int id, HeroClass heroClass, Race race, string heroName)
+        {
+            this.id = id;
+            this._hClass = heroClass;
+            this._race = race;
+            this._heroName = heroName;
+        }
+
+        public override string ToString()
+        {
+
+            return id + " " + (Title.Length == 0 ? "" : (Title + " ")) + _heroName + " " + _hClass + " " + _race + " (hp = " + Hp + "; mana = " + Mana + ";)";
+        }
+
+
+        public string HeroName
+        {
+            get
+            {
+                return _heroName;
+            }
+        }
+
+        public HeroClass HeroClass
+        {
+            get
+            {
+                return _hClass;
+            }
+        }
+
+        public Race Race
+        {
+            get
+            {
+                return _race;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+    }
+}
