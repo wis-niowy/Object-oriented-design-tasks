461a35925ef4f99f50f70943402c8ee37b0ee517
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:22:35 +0200

    Add files via upload

---
+++ lab3/Multiplatform/Solaris/ButtonSolaris.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplatform.Solaris
+{
+    class ButtonSolaris: Interfaces.IButton
+    {
+        private string content;
+        public ButtonSolaris(string cont)
+        {
+            int len = cont.Length;
+            this.content = cont.Substring(0, len / 2) + "Oracle" + cont.Substring(len / 2 + 1, len / 2 -1);
+            Console.WriteLine("Solaris Button created");
+        }
+        public void ButtonPressed()
+        {
+            
+        }
+
+        public void DrawContent()
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
---
+++ lab3/Multiplatform/Solaris/DialogWindowSolaris.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplatform.Solaris
+{
+    class DialogWindowSolaris: Interfaces.IDialogWindow
+    {
+        private string dialogName;
+        private List<Interfaces.ISlider> sliderList;
+        private List<Interfaces.IButton> buttonList;
+
+        public DialogWindowSolaris(string name, List<Interfaces.ISlider> sliderList, List<Interfaces.IButton> buttonList)
+        {
+            this.dialogName = name;
+            this.sliderList = sliderList;
+            this.buttonList = new List<Interfaces.IButton>();
+            this.buttonList.Add(new ButtonSolaris("Abandon all settings"));
+        }
+        public void DrawDialog()
+        {
+            Console.WriteLine("Solaris dialog: {0}", dialogName);
+            foreach (var slider in sliderList)
+            {
+                slider.DrawContent();
+            }
+            
+            Console.WriteLine("You can change these settings but Solaris OS does not support applying them");
+            buttonList[0].DrawContent();
+        }
+    }
+}
