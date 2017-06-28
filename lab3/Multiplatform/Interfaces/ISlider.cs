36696aa8e7a8ddc1bcc1ad9b1a115e28c5b74cc5
Marcin Wiśniewski <marcin.w18@gmail.com>
2017-06-28 15:21:29 +0200

    Add files via upload

---
+++ lab3/Multiplatform/Interfaces/ISlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplatform.Interfaces
+{
+	public interface ISlider
+	{
+		float CurrentValue { get; }
+
+		void ChangeValue(float value);
+
+		void DrawContent();
+	}
+}
